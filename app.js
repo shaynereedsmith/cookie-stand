591671f4c3102438b822e2817952b77aa42659dd
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-14 14:25:36 -0700

    added objects for each of the five shops to give values to location, minCust, maxCust, avgCookieSales

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var storeOne = {
+  location: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSales: 6.3
+};
+
+var storeTwo = {
+  location: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSales: 1.2
+};
+
+var storeThree = {
+  location: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSales: 3.7
+};
+
+var storeFour = {
+  location: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSales: 2.3
+};
+
+var storeFive = {
+  location: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSales: 4.6
+};
