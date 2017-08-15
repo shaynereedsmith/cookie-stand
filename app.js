2079db6364ebcc9ec4b97f8c0fb561ba9153adb0
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-15 15:27:32 -0700

    added an array to store variables for each cookie-store with their unique arguments which are called in a for loop that displays all cookie-stores on the sales.html. and it works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var storeNames = [];
 
 function Store(name, minCust, maxCust, avgCookies) {
   this.name = name;
@@ -15,34 +16,34 @@ function Store(name, minCust, maxCust, avgCookies) {
   this.eachHourSales = function(){
     this.simCookies = [];
     this.totalCookieSales = 0;
-    for (var i = 0; i < this.storeHours.length; i++) {
+    for (var i = 0; i < storeHours.length; i++) {
       var hourlyCookieSales = Math.ceil(this.avgCookies * this.randomCustomersPerHour());
       this.simCookies.push(hourlyCookieSales);
       this.totalCookieSales += hourlyCookieSales;
     }
   };
-};
+  this.dailySalesReport = function(){
+    this.eachHourSales(); // generates the sales we need
+    var location = document.getElementById('sales-section');
+    var locationName = document.createElement('h2');
+    locationName.innerText = this.name;
+    location.appendChild(locationName);
 
-function dailySalesReport(){
-  Store.eachHourSales(); // generates the sales we need
-  var location = document.getElementById('sales-section');
-  var locationName = document.createElement('h2');
-  locationName.innerText = Store.name;
-  location.appendChild(locationName);
+    var list = document.createElement('ul');
+    location.appendChild(list);
 
-  var list = document.createElement('ul');
-  location.appendChild(list);
-
-  for (var i = 0; i < Store.storeHours.length; i++) { // for each hour do some stuff
-    // Store block of code creates a child element for "list"
+    for (var i = 0; i < storeHours.length; i++) { // for each hour do some stuff
+      // this block of code creates a child element for "list"
+      var listItems = document.createElement('li');
+      listItems.innerText = storeHours[i] + ': ' + this.simCookies[i] + ' cookies';
+      list.appendChild(listItems); // adds the new list item to the unordered list
+    }
     var listItems = document.createElement('li');
-    listItems.innerText = Store.storeHours[i] + ': ' + Store.simCookies[i] + ' cookies';
-    list.appendChild(listItems); // adds the new list item to the unordered list
-  }
-  var listItems = document.createElement('li');
-  listItems.innerText = 'Total: ' + Store.totalCookieSales + ' cookies';
-  list.appendChild(listItems);
-}
+    listItems.innerText = 'Total: ' + this.totalCookieSales + ' cookies';
+    list.appendChild(listItems);
+  };
+  storeNames.push(this);
+};
 
 var Pike = new Store('1st and Pike', 23, 65, 6.3);
 var Seatac = new Store('SeaTac Airport', 3, 24, 1.2);
@@ -50,7 +51,11 @@ var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
 var Alki = new Store('Alki', 2, 16, 4.6);
 
-Pike;
+for(var i = 0; i < storeNames.length; i++) {
+  storeNames[i].eachHourSales();
+  storeNames[i].dailySalesReport();
+}
+
 /*
 var pike = {
   storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
