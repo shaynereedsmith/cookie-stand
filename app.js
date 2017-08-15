e44a97d9fc292887104d8313b04c18e9c249aa75
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-14 22:26:23 -0700

    added all 5 stores to the sales.html page. the total calculator still isn't working.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//-------- 1st and Pike --------
+
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var cookieStand1 = {
@@ -37,34 +39,203 @@ for (var j; j < cookieStand1.hourlyCookiesSold.length; j++) {
 totalLi.innerText = 'Total:' + sumOne + ' cookies';
 newLi.appendChild(totalLi);
 
-/*
-var hotel = {
-  name: 'hotel name',
-  checkName: function() {
-    return this.name;
+//-------- SeaTac Airport ---------
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+var cookieStand2 = {
+  location: 'SeaTac Airport',
+  minHourlyCust:  3,
+  maxHourlyCust: 24,
+  avgDailyCustomersHr: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+  },
+  avgCookiesPerSale: 1.2,
+  hourlyCookiesSold: [],
+  hourlyCookiesSoldRate: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var product = this.avgCookiesPerSale * this.avgDailyCustomersHr();
+      this.hourlyCookiesSold.push(Math.round(product));
+    }
   }
 };
 
-var storeTwo = {
-  location: 'SeaTac Airport',
-  minCust: 3,
-  maxCust: 24,
-  avgCookieSales: 1.2
-};
+var stores = document.getElementById('storeList2');
+stores.textContent = cookieStand2.location;
 
-var storeThree = {
+cookieStand2.hourlyCookiesSoldRate();
+for (var i = 0; i < hours.length; i++) {
+  var newLi = document.createElement('li');
+  newLi.innerText = hours[i] + ': ' + cookieStand2.hourlyCookiesSold[i] + ' cookies sold.';
+  storeList2.appendChild(newLi);
+}
+
+var totalLi = document.createElement('li');
+var sumOne = 0;
+for (var j; j < cookieStand2.hourlyCookiesSold.length; j++) {
+  sumOne = sumOne + cookieStand2.hourlyCookiesSold[j];
+}
+totalLi.innerText = 'Total:' + sumOne + ' cookies';
+newLi.appendChild(totalLi);
+
+//-------- Seattle Center --------
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+var cookieStand3 = {
   location: 'Seattle Center',
-  minCust: 11,
-  maxCust: 38,
-  avgCookieSales: 3.7
+  minHourlyCust:  11,
+  maxHourlyCust: 38,
+  avgDailyCustomersHr: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+  },
+  avgCookiesPerSale: 3.7,
+  hourlyCookiesSold: [],
+  hourlyCookiesSoldRate: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var product = this.avgCookiesPerSale * this.avgDailyCustomersHr();
+      this.hourlyCookiesSold.push(Math.round(product));
+    }
+  }
 };
 
-var storeFour = {
+var stores = document.getElementById('storeList3');
+stores.textContent = cookieStand3.location;
+
+cookieStand3.hourlyCookiesSoldRate();
+for (var i = 0; i < hours.length; i++) {
+  var newLi = document.createElement('li');
+  newLi.innerText = hours[i] + ': ' + cookieStand3.hourlyCookiesSold[i] + ' cookies sold.';
+  storeList3.appendChild(newLi);
+}
+
+var totalLi = document.createElement('li');
+var sumOne = 0;
+for (var j; j < cookieStand3.hourlyCookiesSold.length; j++) {
+  sumOne = sumOne + cookieStand3.hourlyCookiesSold[j];
+}
+totalLi.innerText = 'Total:' + sumOne + ' cookies';
+newLi.appendChild(totalLi);
+
+//-------- Capitol Hill --------
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+var cookieStand4 = {
   location: 'Capitol Hill',
-  minCust: 20,
-  maxCust: 38,
-  avgCookieSales: 2.3
+  minHourlyCust:  20,
+  maxHourlyCust: 38,
+  avgDailyCustomersHr: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+  },
+  avgCookiesPerSale: 2.3,
+  hourlyCookiesSold: [],
+  hourlyCookiesSoldRate: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var product = this.avgCookiesPerSale * this.avgDailyCustomersHr();
+      this.hourlyCookiesSold.push(Math.round(product));
+    }
+  }
 };
+
+var stores = document.getElementById('storeList4');
+stores.textContent = cookieStand4.location;
+
+cookieStand4.hourlyCookiesSoldRate();
+for (var i = 0; i < hours.length; i++) {
+  var newLi = document.createElement('li');
+  newLi.innerText = hours[i] + ': ' + cookieStand4.hourlyCookiesSold[i] + ' cookies sold.';
+  storeList4.appendChild(newLi);
+}
+
+var totalLi = document.createElement('li');
+var sumOne = 0;
+for (var j; j < cookieStand4.hourlyCookiesSold.length; j++) {
+  sumOne = sumOne + cookieStand4.hourlyCookiesSold[j];
+}
+totalLi.innerText = 'Total:' + sumOne + ' cookies';
+newLi.appendChild(totalLi);
+
+//-------- Capitol Hill --------
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+var cookieStand4 = {
+  location: 'Capitol Hill',
+  minHourlyCust:  20,
+  maxHourlyCust: 38,
+  avgDailyCustomersHr: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+  },
+  avgCookiesPerSale: 2.3,
+  hourlyCookiesSold: [],
+  hourlyCookiesSoldRate: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var product = this.avgCookiesPerSale * this.avgDailyCustomersHr();
+      this.hourlyCookiesSold.push(Math.round(product));
+    }
+  }
+};
+
+var stores = document.getElementById('storeList4');
+stores.textContent = cookieStand4.location;
+
+cookieStand4.hourlyCookiesSoldRate();
+for (var i = 0; i < hours.length; i++) {
+  var newLi = document.createElement('li');
+  newLi.innerText = hours[i] + ': ' + cookieStand4.hourlyCookiesSold[i] + ' cookies sold.';
+  storeList4.appendChild(newLi);
+}
+
+var totalLi = document.createElement('li');
+var sumOne = 0;
+for (var j; j < cookieStand4.hourlyCookiesSold.length; j++) {
+  sumOne = sumOne + cookieStand4.hourlyCookiesSold[j];
+}
+totalLi.innerText = 'Total:' + sumOne + ' cookies';
+newLi.appendChild(totalLi);
+
+//-------- Alki --------
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+var cookieStand5 = {
+  location: 'Alki',
+  minHourlyCust:  20,
+  maxHourlyCust: 38,
+  avgDailyCustomersHr: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+  },
+  avgCookiesPerSale: 2.3,
+  hourlyCookiesSold: [],
+  hourlyCookiesSoldRate: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var product = this.avgCookiesPerSale * this.avgDailyCustomersHr();
+      this.hourlyCookiesSold.push(Math.round(product));
+    }
+  }
+};
+
+var stores = document.getElementById('storeList5');
+stores.textContent = cookieStand5.location;
+
+cookieStand5.hourlyCookiesSoldRate();
+for (var i = 0; i < hours.length; i++) {
+  var newLi = document.createElement('li');
+  newLi.innerText = hours[i] + ': ' + cookieStand5.hourlyCookiesSold[i] + ' cookies sold.';
+  storeList5.appendChild(newLi);
+}
+
+var totalLi = document.createElement('li');
+var sumOne = 0;
+for (var j; j < cookieStand5.hourlyCookiesSold.length; j++) {
+  sumOne = sumOne + cookieStand5.hourlyCookiesSold[j];
+}
+totalLi.innerText = 'Total:' + sumOne + ' cookies';
+newLi.appendChild(totalLi);
+
+
+/*
 
 var storeFive = {
   location: 'Alki',
